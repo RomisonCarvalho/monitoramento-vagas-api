ff972587f9852731db24a3d525a11754cc11c3ea
Rômison Carvalho <romisonrdjc@gmail.com>
2026-09-03 00:48:06 -0300

    feat: configura PostgreSQL em nuvem com Neon
    - substitui configuração fragmentada do banco por DATABASE_URL
    - configura conexão com PostgreSQL Neon via psycopg
    - adiciona role restrito para uso da API
    - valida leitura e inserção no banco remoto

--- database/conexao.py
+++ database/conexao.py
@@ -1,34 +1,18 @@
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from sqlalchemy.engine import URL
 import os
 
 
 load_dotenv()
 
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
-db_port = int(os.getenv("DB_PORT"))
-db_name = os.getenv("DB_NAME")
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Base(DeclarativeBase):
     pass
-
-DATABASE_URL = URL.create(
-    drivername="postgresql+psycopg",
-    username=db_user,
-    password=db_password,
-    host=db_host,
-    port=db_port,
-    database=db_name
-    )
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def obter_sessao():
